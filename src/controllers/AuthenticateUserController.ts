b762aaf9e75dbb04e2cdfa863df21119e15493c6
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-10-19 20:50:00 -0300

    Autenticação usuario recebendo o codigo

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from "express";
 import { AuthenticateUserService } from "../services/AuthenticateUserService";
 
-
 class AuthenticateUserController {
-    async handle(request: Request, response: Response) {
-        const { code } = request.body;
-        const service = new AuthenticateUserService();
-        const result = await service.execute(code);
-        response.json(result);
+  async handle(request: Request, response: Response) {
+    const { code } = request.body;
+    const service = new AuthenticateUserService();
+    try {
+      const result = await service.execute(code);
+      response.json(result);
+    } catch (error) {
+      response.json({ error: error.message });
     }
+  }
 }
 
-export { AuthenticateUserController }
+export { AuthenticateUserController };
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -8,6 +8,8 @@
  * Retornar uo token com as infos do user
  */
 import axios from "axios";
+import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken";
 
 interface IAccessTokenResponse {
   access_token: string;
@@ -44,7 +46,38 @@ class AuthenticateUserService {
         },
       }
     );
-    return response.data;
+
+    const { login, id, avatar_url, name } = response.data;
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+    if (!user) {
+      await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+    return { token, user };
   }
 }
 
