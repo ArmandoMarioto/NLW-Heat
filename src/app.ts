3e8d9084024d73c58bb88d29dd32ab71aaeb352f
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-10-19 18:52:38 -0300

    Criado rota de login GitHub

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
+import "dotenv/config"
 import express from "express";
 
 const app = express();
+
+app.get('/github', (req, res) => {
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+
+app.get('/signin/callback', (req, res) => {
+    const { code } = req.query;
+
+    return res.json(code);
+
+})
 
 app.listen(4000, () => {
     console.log(`Server is listening on 4000`);
