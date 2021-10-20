d9474ec247bae7f2c5b268d92c257ce9c69d8e08
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-10-19 21:33:20 -0300

    cadastro de mensagem

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
-
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
-router.post('/authenticate', new AuthenticateUserController().handle);
+router.post("/authenticate", new AuthenticateUserController().handle);
 
-export { router }
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
+
+export { router };
