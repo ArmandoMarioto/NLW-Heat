de0560c95f2dae0a5147a91be6d608a5741a50bb
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-10-19 22:06:41 -0300

    Retornando as 3 ultimas mensagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
-
 
 const router = Router();
 
@@ -13,5 +13,7 @@ router.post(
   ensureAuthenticated,
   new CreateMessageController().handle
 );
+
+router.get("/messages/last3", new GetLast3MessagesController().handle);
 
 export { router };
