b6d17adc829991a59ebb9e79069d1e66a0d725e3
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-11-03 08:08:18 -0300

    Colocando Scss, mas ainda falta arrumar o formulario.

--- web/src/components/SendMessageForm/index.tsx
+++ web/src/components/SendMessageForm/index.tsx
@@ -1,3 +1,36 @@
+import { useContext } from "react";
+import { VscGithubInverted, VscSignOut } from "react-icons/vsc";
+import { AuthContext } from "../../contexts/auth";
+import styles from "./styles.module.scss";
 export function SendMessageForm() {
-  return <h1>SendMessageForm</h1>;
+  const { user } = useContext(AuthContext);
+  return (
+    <div className={styles.sendMessageFormWrapper}>
+      <button className={styles.signOutButton}>
+        <VscSignOut size="32" />
+      </button>
+
+      <header className={styles.userInformation}>
+        <div className={styles.userImage}>
+          <img src={user?.avatar_url} alt={user?.name} />
+        </div>
+        <strong className={styles.userName}>{user?.name}</strong>
+        <span className={styles.userGithub}>
+          <VscGithubInverted size="16" />
+          {user?.login}
+        </span>
+      </header>
+
+      <form className={styles.sendMessageForm}>
+        <label htmlFor="message">Mensagem</label>
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Qual sua expectativa para o evento?"
+        />
+
+        <button type="submit">Enviar mensagem</button>
+      </form>
+    </div>
+  );
 }
